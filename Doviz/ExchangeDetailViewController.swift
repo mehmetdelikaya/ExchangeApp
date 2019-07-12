15a7ed66ce5177d7410fb7e2064ce3cd3a3a029b
Mehmet <mehmet.delikaya@gmail.com>
2019-07-12 16:07:49 +0300

    calculate and reverse implemented

--- Doviz/ExchangeDetailViewController.swift
+++ Doviz/ExchangeDetailViewController.swift
@@ -14,9 +14,12 @@ class ExchangeDetailViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
+
+        view.addGestureRecognizer(tap)
         
         labelSelectedCurrency.text = listItem?.name
+        labelCurrencyName.text = listItem?.name
         
         if let flagName = listItem?.imageName{
             imgSelected.isHidden = false
@@ -29,15 +32,74 @@ class ExchangeDetailViewController: UIViewController {
         let gestureSwift2AndHigher = UITapGestureRecognizer(target: self, action:  #selector (self.tapped (_:)))
         self.viewBack.addGestureRecognizer(gestureSwift2AndHigher)
         
+        imgTarget.image = UIImage(named: "TR")
 
+        
+        calculate(option: shouldMultiply)
+        
+    
+    }
+    @objc func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
     }
     
+    func calculate(option shouldMultiply: Bool){
+        if let amountEntered = textViewAmount.text,
+            let buyingPrice = listItem?.buying,
+            let sellingPrice = listItem?.selling {
+            
+            let doubleAmountEntered = Double(amountEntered)
+            let doubleBuyingPrice = Double(buyingPrice)
+            let doubleSellingPrice = Double(sellingPrice)
+            
+            if shouldMultiply{
+                labelTargetBuying.text = String(format: "Alış: %.2f", doubleAmountEntered! * doubleBuyingPrice!)
+                labelTargetSelling.text = String(format:"Satış:  %.2f", doubleAmountEntered! * doubleSellingPrice! )
+            } else {
+                labelTargetBuying.text = String(format: "Alış: %.2f", doubleAmountEntered! / doubleBuyingPrice!)
+                labelTargetSelling.text = String(format:"Satış:  %.2f", doubleAmountEntered! / doubleSellingPrice! )
+            }
+            
+        }
+        
+    }
     
+    var shouldMultiply = true
+    
+    func reverse(){
+        
+        shouldMultiply = !shouldMultiply
+        
+        labelSelectedCurrency.text = "Turkish Liras"
+        labelCurrencyName.text = "Turkish Liras"
+        imgSelected.image = UIImage(named: "TR")
+        
+        if let flagName = listItem?.imageName{
+             imgTarget.isHidden = false
+             imgTarget.image = UIImage(named: flagName)
+        }else{
+             imgTarget.isHidden = true
+        }
+        
+        calculate(option: shouldMultiply)
+    }
+    
+    @IBAction func buttonCalculate(_ sender: Any) {
+        calculate(option: shouldMultiply)
+    }
+    @IBAction func buttonReverse(_ sender: Any) {
+        reverse()
+    }
     @IBOutlet weak var viewBack: UIView!
     @IBOutlet weak var imgSelected: UIImageView!
     @IBOutlet weak var labelSelectedCurrency: UILabel!
     @IBOutlet weak var textViewAmount: UITextView!
+    @IBOutlet weak var labelCurrencyName: UILabel!
     
+    @IBOutlet weak var labelTargetSelling: UILabel!
+    @IBOutlet weak var labelTargetBuying: UILabel!
+    @IBOutlet weak var imgTarget: UIImageView!
     var listItem : ListModel?
    
     @objc func tapped(_ sender:UITapGestureRecognizer){
